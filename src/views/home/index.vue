4a308e726c0da5f675dca7654f3df44342ae41d7
dirunru <15514209607@163.com>
2024-06-27 08:48:49 +0800

    feat:1

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,42 @@
 <template>
-  <div>
-      <h1>Home</h1>
-  </div>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+    <el-footer>Footer</el-footer>
+  </el-container>
 </template>
 
 <script setup lang='ts'>
 
 </script>
 
-<style lang="less" scoped>
-
+<style lang="scss" scoped>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
 </style>
